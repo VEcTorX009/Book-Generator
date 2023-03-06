dd29951a3afeabb0ff77c13a1a5a9f417cde22fc
Som <aishwarysrivastava2006@gmail.com>
2023-03-06 19:31:37 +0530

    hello

--- pages/api/generate-books.js
+++ pages/api/generate-books.js
@@ -14,7 +14,6 @@ export default async function (req, res) {
     temperature: 0.6,
     max_tokens: 2048,
   });
-  console.log(completion.data.choices[0].text)
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
